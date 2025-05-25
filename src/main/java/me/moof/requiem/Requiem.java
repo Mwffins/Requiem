9b78c8ce7b871fa0b6710a4e44561080f15fb721
Moof <111189250+Mwffins@users.noreply.github.com>
2025-05-25 07:33:42 +0100

    Initial commit

---
+++ src/main/java/me/moof/requiem/Requiem.java
@@ -0,0 +1,17 @@
+package me.moof.requiem;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Requiem implements ModInitializer {
+	public static final String MOD_ID = "requiem";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/me/moof/requiem/RequiemClient.java
@@ -0,0 +1,10 @@
+package me.moof.requiem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RequiemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
